7b970ef6d301197c534b5777bd0ba3e17711109c
HienDX124 <xuanhien2000678354@gmail.com>
2023-04-04 22:20:43 +0700

    import sprites character, sprites tilemap, import some LTA framework

---
+++ Assets/LTA/Packages/UseItemHandles/ItemChacraterHandle/Runtime/HoldItemHandle.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LTA.Base.Item;
+
+[System.Serializable]
+public class HoldItemInfo
+{
+    public string path = "";
+    public string holdObjectName = "";
+    public Vector3 localPos = Vector3.zero;
+}
+
+
+public class HoldItemHandle : BaseUseItemEffect<HoldItemInfo>,IAddItemsInfo,IEndLoadAnimation
+{
+    public void OnAddItemsInfo(int index, PackItem item, UseItemController useItem)
+    {
+        throw new System.NotImplementedException();
+    }
+    GameObject holdItem;
+    public void OnEndLoadAnimtion()
+    {
+        GameObject prefabItem = Resources.Load<GameObject>(ItemInfo.path);
+        Transform holdItemPos = transform.Find(ItemInfo.holdObjectName);
+        holdItem = Instantiate(prefabItem, holdItemPos);
+        holdItem.transform.localPosition = ItemInfo.localPos;
+    }
+    private void OnDestroy()
+    {
+        if (holdItem != null)
+            Destroy(holdItem);
+    }
+}
---
+++ Assets/_Scripts/Controller/Player/PlayerMoveController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using LTA.BasicCharacter.Character2D.Platform;
+using UnityEngine;
+
+public class PlayerMoveController : MoveController
+{
+    public override void Move(Vector3 direction)
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
