d3a39a4c97e048dea48a511804d7b150f435d1f4
imgiang <79862204+imgiang@users.noreply.github.com>
2023-04-13 02:43:00 +0700

    add joystick for player

--- Assets/_Scripts/Controller/Player/PlayerController.cs
+++ Assets/_Scripts/Controller/Player/PlayerController.cs
@@ -1,24 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerMoveController moveController;
     [SerializeField] private Transform tranShoot;
+    [SerializeField] private JoyStickContoller joystick;
     private int count = 0;
+    private Vector3 direction;
     private void Update()
     {
         count++;
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveController.Move(Vector3.right);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveController.Move(Vector3.left);
-        }
-
+        direction = new Vector3(joystick.Direction.x,0);
+        moveController.Move(direction);
+        
         if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
         {
             moveController.StopMove();
